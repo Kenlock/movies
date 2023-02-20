8f8f920f38dc902630d3144085c9b03396b824d6
KEVIN OTIENO <colvinbrown201@gmail.com>
2023-02-20 12:32:51 +0300

    initial commit

---
+++ src/main/java/com/integriobits/moviese/MovieseApplication.java
@@ -0,0 +1,31 @@
+package com.integriobits.moviese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class MovieseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieseApplication.class, args);
+	}
+
+  @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
+
+
+}
